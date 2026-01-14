0ef24f348661e598772422a8783c3f3393bcc4c1
Aniruddha Shastrakar <aniruddhashastrakar1612@gmail.com>
2026-01-14 22:26:54 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+class Demo {
+    int x = 10;
+    int display() {
+        System.out.println("x = " + x);
+        return 0;
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Demo D1 = new Demo();
+        System.out.println(D1);
+        System.out.println(D1.display());
+    }
+}
